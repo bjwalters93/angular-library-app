a3f782f075adfd81896c80b341a5d54e6924e890
Brian Walters <waltersbrian68@gmail.com>
2025-05-31 11:46:18 -0400

    working out routing

--- TODO.md
+++ TODO.md
@@ -1,3 +1,7 @@
 # To DO
 
-1. Incrementally add authentication/authorization (user login, user register, read/write permissions).
+1. Step one is to set up routes.
+
+2. PrimeNG - start getting into prime ng form forms and buttons.
+
+- Incrementally add authentication/authorization (user login, user register, read/write permissions).
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Books Collection
+  { path: 'books', component: FirstComponent },
+  { path: 'books/new', component: SecondComponent },
+  { path: 'books/:id', component: SecondComponent },
+  { path: 'books/:id/edit', component: SecondComponent },
+  //   Patrons Collection
+  { path: 'patrons', component: SecondComponent },
+  { path: 'patrons/new', component: SecondComponent },
+  { path: 'patrons/:id', component: SecondComponent },
+  //   Loans Collection
+  { path: 'loans', component: SecondComponent },
+  { path: 'loans/new', component: SecondComponent },
+  { path: 'loans/overdue', component: SecondComponent },
+  //   Search with query parameters
+  { path: 'books/search', component: SecondComponent },
+  { path: 'patrons/search', component: SecondComponent },
+];
